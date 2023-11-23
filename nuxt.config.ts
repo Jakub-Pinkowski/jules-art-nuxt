3161948790460571fa8f0190597238332a6c5e77
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-23 16:51:44 +0100

    Add ignore rules for image files and update Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
     modules: ['@pinia/nuxt'],
+    css: ['~/assets/css/main.css'],
+    app: {
+        head: {
+            title: 'Jules-Art',
+            meta: [
+                // Add other meta tags as needed
+                { charset: 'utf-8' },
+                { name: 'description', content: 'Jules Art artist portfolio' },
+                { property: 'og:title', content: 'Jules-Art' },
+                { property: 'og:description', content: 'Jules Art artist portfolio' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            ],
+        },
+    },
 })
