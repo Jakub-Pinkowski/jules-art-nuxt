acabdcfd95e4d55a00cf459ab6568081088b8024
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-23 21:19:10 +0100

    Update navbar links in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,19 +3,18 @@
         <header>
             <nav class="navbar bg-base-100">
                 <div class="flex-1">
-                    <a class="btn btn-ghost text-xl">Jules-Art</a>
+                    <nuxt-link class="btn btn-ghost text-2xl" to="/">Jules-Art</nuxt-link>
                 </div>
                 <ThemeToggle />
                 <div class="flex-none">
-                    <ul class="menu menu-horizontal px-1">
+                    <ul class="menu menu-horizontal px-1 text-xl">
                         <li><nuxt-link to="/">Home</nuxt-link></li>
-                    <li><nuxt-link to="/photos">Photos</nuxt-link></li>
-                    <li><nuxt-link to="/movies">Movies</nuxt-link></li>
-                    <li><nuxt-link to="/movies">Reels</nuxt-link></li>
-                    <li><nuxt-link to="/resources">Resources</nuxt-link></li>
+                        <li><nuxt-link to="/photos">Photos</nuxt-link></li>
+                        <li><nuxt-link to="/movies">Movies</nuxt-link></li>
+                        <li><nuxt-link to="/movies">Reels</nuxt-link></li>
+                        <li><nuxt-link to="/resources">Resources</nuxt-link></li>
                     </ul>
                 </div>
-
             </nav>
         </header>
         <slot />
