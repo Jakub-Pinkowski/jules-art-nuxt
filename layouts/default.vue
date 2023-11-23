405b8ed444b582df17999509e5e9809c8258b3a7
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-23 21:16:44 +0100

    Update navigation styling and links in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,21 @@
 <template>
     <div>
         <header>
-            <nav class="flex place-content-between border-b py-4 mt-4 mb-4">
-                <div>
-                    <h1 class="text-2xl font-bold mx-4">My Site</h1>
+            <nav class="navbar bg-base-100">
+                <div class="flex-1">
+                    <a class="btn btn-ghost text-xl">Jules-Art</a>
                 </div>
-
                 <ThemeToggle />
-
-                <ul class="flex m-2 py-2 px-2">
-                    <li><nuxt-link to="/">Home</nuxt-link></li>
+                <div class="flex-none">
+                    <ul class="menu menu-horizontal px-1">
+                        <li><nuxt-link to="/">Home</nuxt-link></li>
                     <li><nuxt-link to="/photos">Photos</nuxt-link></li>
                     <li><nuxt-link to="/movies">Movies</nuxt-link></li>
                     <li><nuxt-link to="/movies">Reels</nuxt-link></li>
                     <li><nuxt-link to="/resources">Resources</nuxt-link></li>
-                </ul>
+                    </ul>
+                </div>
+
             </nav>
         </header>
         <slot />
@@ -25,8 +26,6 @@
     </div>
 </template>
 
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <style scoped lang="scss"></style>
