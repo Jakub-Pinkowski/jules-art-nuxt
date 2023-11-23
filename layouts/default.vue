5f2ccfd36fe3365b3a36a97221ac533094b9f058
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-23 21:36:03 +0100

    Add GitHub icon and update footer layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,13 +18,21 @@
             </nav>
         </header>
         <slot />
-        <footer>
-            <p>&copy; 2023</p>
-            <button class="btn btn-primary">Test Button</button>
+        <footer class="footer items-center p-4 bg-neutral text-neutral-content">
+            <aside class="items-center grid-flow-col">
+                <p>© 2023 Jakub Pinkowski</p>
+            </aside>
+            <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+                <nuxt-link to="https://github.com/Jakub-Pinkowski?tab=repositories">
+                    <img :src="github_icon" alt="GitHub Icon" />
+                </nuxt-link>
+            </nav>
         </footer>
     </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import github_icon from '~/assets/icons/github_icon.svg'
+</script>
 
 <style scoped lang="scss"></style>
