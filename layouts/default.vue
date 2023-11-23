ad9a0096723485388ea9685de60073eaeedcf4a7
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-23 16:41:19 +0100

    Refactor app.vue layout

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Layout here</h2>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/photos.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Photos</h1>
+    </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped lang="scss"></style>
