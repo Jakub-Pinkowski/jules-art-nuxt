305ae91e6c4306ffc0b51694bdff6de30c8f06f2
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-23 16:57:26 +0100

    Update navigation links in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,20 @@
 <template>
-  <div>
-    <h2>Layout here</h2>
-    <slot />
-  </div>
+    <div>
+        <header>
+            <nav>
+                <ul>
+                    <li><nuxt-link to="/">Home</nuxt-link></li>
+                    <li><nuxt-link to="/photos">Photos</nuxt-link></li>
+                    <li><nuxt-link to="/movies">Movies</nuxt-link></li>
+                    <li><nuxt-link to="/movies">Reels</nuxt-link></li>
+                    <li><nuxt-link to="/resources">Resources</nuxt-link></li>
+                </ul>
+            </nav>
+        </header>
+        <slot />
+    </div>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup></script>
 
-</script>
-
-<style>
-
-</style>
+<style></style>
