296a701c44649d729f42625b8dc5703894aa9ce8
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-24 20:14:15 +0100

    Refactor footer component

---
+++ components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer class="flex place-content-between items-center p-2 bg-neutral text-neutral-content">
+        <aside class="items-center">
+            <p>© 2023 Jakub Pinkowski</p>
+        </aside>
+        <nuxt-link to="https://github.com/Jakub-Pinkowski" class="justify-self-end">
+            <img :src="github_icon" alt="GitHub Icon" />
+        </nuxt-link>
+    </footer>
+</template>
+
+<script lang="ts" setup>
+import github_icon from '~/assets/icons/github_icon.svg'
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -8,21 +8,12 @@
         <main class="flex-1 mx-6">
             <slot />
         </main>
-        <footer class="footer items-center p-2 bg-neutral text-neutral-content">
-            <aside class="items-center grid-flow-col">
-                <p>© 2023 Jakub Pinkowski</p>
-            </aside>
-            <nav class="md:place-self-center md:justify-self-end">
-                <nuxt-link to="https://github.com/Jakub-Pinkowski">
-                    <img :src="github_icon" alt="GitHub Icon" />
-                </nuxt-link>
-            </nav>
-        </footer>
+
+        <Footer />
     </div>
 </template>
 
 <script lang="ts" setup>
-import github_icon from '~/assets/icons/github_icon.svg'
 </script>
 
 <style scoped lang="scss"></style>
