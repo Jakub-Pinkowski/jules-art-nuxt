95d12d875a9baccaf888aeff7f7e0720a15b2698
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-24 11:25:44 +0100

    Refactor reels.vue component

--- pages/reels.vue
+++ pages/reels.vue
@@ -43,13 +43,104 @@
 </template>
 
 <script lang="ts" setup>
-import { Swiper, SwiperSlide } from 'swiper/vue'
-import { Navigation } from 'swiper/modules'
+import { ref, onMounted } from 'vue'
+import { useReelsStore } from '~/stores/reels'
 
 import 'swiper/css'
 import 'swiper/css/effect-coverflow'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
+
+const reelsStore = useReelsStore()
+const reels = reelsStore.reels
+const reversedReels = reels.slice().reverse()
+
+// Desktop
+const onSlideChangeTransitionEnd = (swiper: any) => {
+    const videos = Array.from(document.querySelectorAll('.desktop video')) as HTMLVideoElement[]
+    videos.forEach((video, index) => {
+        if (index === swiper.activeIndex) {
+            video.classList.remove('inactive')
+            video.setAttribute('controls', '')
+        } else {
+            video.classList.add('transition')
+            video.classList.add('inactive')
+            video.removeAttribute('controls')
+            video.pause()
+            video.removeAttribute('autoplay')
+        }
+    })
+}
+// Apply inactive classes to all the videos except the first one when page loads
+onMounted(() => {
+    const videos = Array.from(document.querySelectorAll('.desktop video')) as HTMLVideoElement[]
+    videos.forEach((video, index) => {
+        if (index === 0) {
+            video.classList.remove('transition')
+            video.classList.remove('inactive')
+            video.setAttribute('controls', '')
+        } else {
+            video.classList.add('transition')
+            video.classList.add('inactive')
+            video.removeAttribute('controls')
+        }
+    })
+})
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+/* Desktop */
+.swiper {
+    width: 100%;
+    height: 100%;
+
+    .swiper-slide {
+        background-position: center;
+        background-size: cover;
+        width: 400px;
+        height: auto;
+
+        img,
+        video {
+            display: block;
+            width: 84%;
+            height: auto;
+            transition: filter 0.3s ease-out;
+        }
+
+        video.inactive {
+            filter: blur(4px);
+        }
+
+        .video-text {
+            position: absolute;
+            bottom: 60px;
+            left: -24px;
+            width: 100%;
+            text-align: center;
+            color: var(--light-gray);
+            font-size: 1.5rem;
+            font-weight: 600;
+        }
+    }
+
+    .swiper-button-next {
+        color: var(--main-bg-color);
+        height: 50px !important;
+        width: 50px !important;
+        background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%234e4e50' viewBox='0 0 8 8'%3E%3Cpath d='M2.75 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z'/%3E%3C/svg%3E") !important;
+    }
+
+    .swiper-button-prev {
+        color: var(--main-bg-color);
+        height: 50px !important;
+        width: 50px !important;
+        background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%234e4e50' viewBox='0 0 8 8'%3E%3Cpath d='M5.25 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z'/%3E%3C/svg%3E") !important;
+    }
+
+    .swiper-button-next::after,
+    .swiper-button-prev::after {
+        content: '';
+    }
+}
+</style>
