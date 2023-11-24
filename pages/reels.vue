3873f6a4e522f49da7e415384f51835d157adfda
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-11-24 12:08:03 +0100

    Add @nuxt/test-utils and vitest dependencies

--- pages/reels.vue
+++ pages/reels.vue
@@ -123,13 +123,13 @@ onMounted(() => {
     }
     .swiper-button-next {
         height: 50px;
-        width: 50px ;
+        width: 50px;
         background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%234e4e50' viewBox='0 0 8 8'%3E%3Cpath d='M2.75 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z'/%3E%3C/svg%3E") !important;
     }
 
     .swiper-button-prev {
-        height: 50px ;
-        width: 50px ;
+        height: 50px;
+        width: 50px;
         background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%234e4e50' viewBox='0 0 8 8'%3E%3Cpath d='M5.25 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z'/%3E%3C/svg%3E") !important;
     }
 
